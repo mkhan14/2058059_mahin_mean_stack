b11cb4ec2ee44f773703433a69794f8d9e29aaac
mkhan14 <mkhan6241@bths.edu>
2021-08-23 17:24:11 -0400

    debugger

--- Phase 3/log-records/log.js
+++ Phase 3/log-records/log.js
@@ -4,7 +4,8 @@ let readline=require("readline-sync");
 let f_name=readline.question("Enter your first name: ");
 let l_name=readline.question("Enter your last name: ");
 let gen=readline.question("Enter your gender: ");
-let emailId=readline.questionEMail("Enter your email id: ")
+let emailId=readline.questionEMail("Enter your email id: ");
+debugger;
 dt=Date().toLocaleString();
 
 let users=JSON.parse(fs.readFileSync("users.json").toString());
@@ -12,4 +13,5 @@ users.push({first_name:f_name,last_name:l_name,gender:gen,email:emailId,date_tim
 fs.writeFileSync("users.json",JSON.stringify(users));
 
 console.log("Stored new user");
+debugger;
 console.log(users);
